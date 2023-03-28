e27a96b68ad5bf32204ac5e04e0c3498fdf5b31e
Esteban <ext.evilches@servipag.cl>
2023-03-28 18:37:51 -0300

    FEAT(): se agrega sidebar

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout-page.component.css']
 })
 export class LayoutPageComponent {
-
+  public sideBarItems = [
+    { label: 'Listado', icon: 'label',  url: './list' },
+    { label: 'Añadir',  icon: 'add',    url: './new-hero' },
+    { label: 'Buscar',  icon: 'search', url: './search' },
+  ]
 }
